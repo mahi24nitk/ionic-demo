6c65e066ca9ccad674bfdd16519a90652eca7b48
Mahendra <mahendra@fbc.nyc>
2021-07-20 17:05:32 +0530

    url changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic12345.starter',
+  appId: 'io.ionic123456.starter',
   appName: 'propertydek-test',
   webDir: 'build',
   bundledWebRuntime: false
